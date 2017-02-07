d8739644441fe50cc6f413caf5f6bd4b91a645c8
palvarezc <paula.alvarez@cern.ch>
2017-02-07 15:30:11 +0000

    Deleting things

--- toy_result_12_07/cleanup.sh
+++
@@ -1,6 +0,0 @@
-hadd -k -f /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07/toystudyHistBremCatTsallisBkg_resultsMode7L0ETOSOnly_d.root /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07/output_1/toystudyHistBremCatTsallisBkg_resultsMode7L0ETOSOnly_d.root
-mkdir iodir
-mv runOnBatch.sh.* iodir/.
-/home/hep/palvare1/B2Kmumu_Kee/Sulley//bin/maintables /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07/toystudyHistBremCatTsallisBkg_resultsMode7L0ETOSOnly_d.root params_L0ETOSOnly_d 199 92 440 16 /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07//toyresults.dat 0
-/home/hep/palvare1/B2Kmumu_Kee/Sulley//bin/maintables /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07/toystudyHistBremCatTsallisBkg_resultsMode7L0ETOSOnly_d.root params_L0ETOSOnly_d 199 92 440 16 /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07//toyresults.tex 1
-pdflatex /home/hep/palvare1/B2Kmumu_Kee/Sulley/toy_result_12_07//toyresults.tex
